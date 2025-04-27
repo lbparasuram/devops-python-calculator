0e34e22753aa13c8d9da0eca1c2a639d74e84f89
Lakshmi Bhavani Parasuram <lakshmi@Lakshmis-MacBook-Air.local>
2025-04-27 20:07:37 +0100

    added subtract functionality

--- tests/test_calc_func.py
+++ tests/test_calc_func.py
@@ -8,3 +8,8 @@ NUMBER_2 = 2.0
 def test_add():
     value = add(NUMBER_1, NUMBER_2)
     assert value == 5.0
+
+
+def test_subtract():
+    value = subtract(NUMBER_1, NUMBER_2)
+    assert value == 1
